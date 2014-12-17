3acda89505b96136a4c042f54f004db70303ac63
Chakrit Wichian <service@chakrit.net>
2014-12-17 18:28:07 +0700

    Use TaskCompletionSource for EventWaiter which is more reliable.

--- LyncRPC/EventWaiter.cs
+++ LyncRPC/EventWaiter.cs
@@ -5,44 +5,20 @@ using System.Threading.Tasks;
 
 namespace LyncRPC
 {
-    public class EventWaiter<T>: IDisposable where T: EventArgs
+    public class EventWaiter<T>: TaskCompletionSource<T> where T: EventArgs
     {
-        private ManualResetEventSlim _eventTriggered = new ManualResetEventSlim (false);
-        private T _eventArgs = null;
-        private Task<T> _task;
+        private Action<EventHandler<T>> _remover = null;
 
-        public T EventArgs { get { return _task.Result; } }
-
-        public Task<T> Task { get { return _task; } }
-
-        public EventWaiter (Action<EventHandler<T>> adder, Action<EventHandler<T>> remover)
+        public EventWaiter (Action<EventHandler<T>> adder, Action<EventHandler<T>> remover) : base ()
         {
+            _remover = remover;
             adder (trigger);
-            _task = new Task<T> (() => {
-                _eventTriggered.Wait ();
-                remover (trigger);
-                return _eventArgs;
-            });
         }
-
-        public void Dispose ()
-        {
-            _eventTriggered.Dispose ();
-            _eventTriggered = null;
-            _eventArgs = null;
-        }
-
-
-        public void Wait ()
-        {
-            _task.Wait ();
-        }
-
 
         private void trigger (object sender, T e)
         {
-            _eventArgs = e;
-            _eventTriggered.Set ();
+            SetResult (e);
+            _remover (trigger);
         }
     }
 }
